0a23ea85a0eccf082a586e3c7f7caccfb8c8e1d7
Manish <manishmamgain7236672366@gmail.com>
2023-07-30 23:32:35 +0530

    class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// rcc -> react class based component 
+import React, { Component } from 'react'
+// previously we made textUtils on the basis of function based component and now this one have class based component as you can see below is the class based component.
+export default class App extends Component {
+  m = "manish";
+  render() {
+    return (
+      <div>App is working fine {this.m}</div>
+    )
+  }
 }
 
-export default App;
+
+// Your API key is: 5f3863ab0e294d3a89c3097826ba64bb

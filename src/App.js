767ec88b7aff55b3b35261e3ecfc9cec0544e433
Manish <manishmamgain7236672366@gmail.com>
2023-07-31 22:28:24 +0530

    Looping through an array in JSX to display NewsItems from state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import NewsItem from './components/NewsItem';
 // previously we made textUtils on the basis of function based component and now this one have class based component as you can see below is the class based component.
 // render () renders all the html css whatever present there.
 export default class App extends Component {
@@ -20,3 +21,5 @@ export default class App extends Component {
 
 
 // Your API key is: 5f3863ab0e294d3a89c3097826ba64bb
+// https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5f3863ab0e294d3a89c3097826ba64bb put this so then you can see the api result for news.
+// &q=cricket add this so then you can specific find api for a specific query video number 24 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class NewsItem extends Component {
-  static propTypes = {
-
-  }
+ 
+  
 
   render() {
+    let {title , description ,urlToImage , newsUrl} = this.props;
     return (
-      <div>
-        This is a news item
+      <div className='container my-3'>
+        <div className="card" style={{width: "18rem"}}>
+        <img src={urlToImage} className="card-img-top" alt="ab padi sanch"/>
+        <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target = "blank" className="btn btn-sm btn-success">Read More</a> {/* target = "blank" means open  link in new tab*/}
+        </div>
+        </div>
       </div>
     )
   }
